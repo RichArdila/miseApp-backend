04761683f0b700c6adcc5aff359a09fb7b2bfb1c
Richard <richardardilaz17@hotmail.com>
2025-10-14 07:57:14 -0400

    created updateStations function

--- src/controllers/stationController.js
+++ src/controllers/stationController.js
@@ -26,3 +26,44 @@ export const getStation = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+// create updateStation
+export const updateStation = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, description } = req.body;
+
+    let updateFields = [];
+    let values = [];
+
+    if (name) {
+      updateFields.push("name = $" + (values.length + 1));
+      values.push(name);
+    }
+
+    if (description) {
+      updateFields.push("description = $" + (values.length + 1));
+      values.push(description);
+    }
+
+    if (values.length === 0) {
+      return res.status(400).json({ error: "No fields to update" });
+    }
+
+    values.push(id);
+
+    const query = `UPDATE stations SET ${updateFields.join(", ")} WHERE id = $${
+      values.length
+    }
+    RETURNING id, name, description`;
+
+    const result = await pool.query(query, values);
+    if (result.rowCount === 0) {
+      return res.status(400).json({ error: "Station not found" });
+    }
+    return res.json(result.rows[0]);
+  } catch (error) {
+    return res.status(500).json({ error: "Error updating station" });
+  }
+};
+// create deleteStation
--- src/routes/stations.js
+++ src/routes/stations.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { createStation, getStation } from "../controllers/stationController.js";
+import {
+  createStation,
+  getStation,
+  updateStation,
+} from "../controllers/stationController.js";
 import { authMiddelware, authorizeRole } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/", authMiddelware, authorizeRole("admin"), createStation);
 router.get("/", authMiddelware, getStation);
+router.patch("/:id", authMiddelware, authorizeRole("admin", updateStation));
 
 export default router;
